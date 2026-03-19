872cba9a9d029844ecdffe78f7aab11c24f38488
Mahendra Kushwaha <140656643+mahendrakumarkushwaha03@users.noreply.github.com>
2026-03-19 09:35:08 +0530

    Add student name input and modify pass/fail output

--- cheeta6.java
+++ cheeta6.java
@@ -1,16 +1,20 @@
+//6. Declare student name (String) and marks (int); print “Pass” if marks ≥ 40, otherwise print “Fail”.
+
 package Jackie;
 import java.util.Scanner;
 public class cheeta6 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Student Name : ");
+		string Name=sc.next();
 		System.out.println("Enter the marks : ");
 		int marks = sc.nextInt();
 		if (marks>=40) {
-			System.out.println("pass");
+			System.out.println(Name + " is pass");
 		}else
 		{
-			System.out.println("fail");
+			System.out.println(Name + " is fail");
 		}
 	}
 }
