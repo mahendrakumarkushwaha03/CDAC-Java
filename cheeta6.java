d53fe598437819e93b34929c891545c0d99fa07c
Mahendra Kushwaha <140656643+mahendrakumarkushwaha03@users.noreply.github.com>
2026-03-19 09:38:09 +0530

    Update cheeta6.java

--- cheeta6.java
+++ cheeta6.java
@@ -1,5 +1,3 @@
-//6. Declare student name (String) and marks (int); print “Pass” if marks ≥ 40, otherwise print “Fail”.
-
 package Jackie;
 import java.util.Scanner;
 public class cheeta6 {
@@ -7,7 +5,7 @@ public class cheeta6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Student Name : ");
-		string Name=sc.next();
+		String Name=sc.next();
 		System.out.println("Enter the marks : ");
 		int marks = sc.nextInt();
 		if (marks>=40) {
